edc138baf325c0dc60143c14cb370731484ac7f9
chandryou <applegna@gmail.com>
2019-05-29 18:01:20 +0900

    revised the extra code to summarize the result

--- extras/secondaryOutcomePlotting.r
+++ extras/secondaryOutcomePlotting.r
@@ -1,40 +1,50 @@
 library(forestplot)
 library(dplyr)
 
-outputFolder<-"/Users/chan/OneDrive/Study/Ticagrelor/Result/190315"
-cmOutput<-file.path(outputFolder, "cmOutput")
-outcomeModelRef<-readRDS(file.path(cmOutput,"outcomeModelReference.rds"))
-analysisSummary<-read.csv(file.path(outputFolder,"analysisSummary.csv"),stringsAsFactors =F)
+outputFolder<-""
+analysisSummary <- read.csv(file.path(outputFolder,"cohort_method_result.csv"),stringsAsFactors = FALSE)
+analysis <- read.csv(file.path(outputFolder,"cohort_method_analysis.csv"),stringsAsFactors = FALSE)
+
+outcomes <- read.csv(file.path(outputFolder,"outcome_of_interest.csv"),stringsAsFactors = FALSE)
+#covBal <- read.csv(file.path(outputFolder,"covariate_balance.csv"),stringsAsFactors = FALSE)
+
+#Camel To Snake
+colnames(analysisSummary)<-SqlRender::snakeCaseToCamelCase(colnames(analysisSummary))
+colnames(analysis)<-SqlRender::snakeCaseToCamelCase(colnames(analysis))
+colnames(outcomes)<-SqlRender::snakeCaseToCamelCase(colnames(outcomes))
+#colnames(covBal)<-SqlRender::snakeCaseToCamelCase(colnames(covBal))
 
 
-
-outcomeIds<-unique(outcomeModelRef$outcomeId)
-analysisIds <- c(1:4,7:12) #unique(analysisSummary$analysisId)
-
-#outcomeOfInterest <- unique(outcomeModelRef$outcomeId[outcomeModelRef$outcomeOfInterest==TRUE])
+outcomeOfInterest <- outcomes$outcomeId
 outcomeNameOfInterest <- c("NACE","IschemicEvent","ischemicStroke","Revascularization","AMI",
                            "HemorrhagicEvent","hemorrhagicStroke","giBleeding",
                            "anyDeath","dyspnea")
+outcomeIdOfInterest<-outcomes$outcomeId[outcomes$outcomeName %in% outcomeNameOfInterest]
+outcomeIdOrder<-outcomes$outcomeId [match(outcomeNameOfInterest,outcomes$outcomeName)]
 
 outcomeNameOfInterestNarrow <- c("NACE_narrow","IschemicEvent_narrow","ischemicStroke_narrow","AMI_narrow",
-                               "HemorrhagicEvent_narrow","ICH_narrow","giBleeding_narrow")
+                                 "HemorrhagicEvent_narrow","ICH_narrow","giBleeding_narrow")
 
-unique(analysisSummary$outcomeName)
+outcomeNameOrder <- c("Net Adverse Clinical Event", 
+                      
+                      "Ischemic event",
+                      "Ischemic stroke",
+                      "Revascularization",
+                      "Acute MI",
+                      
+                      "Hemorrhagic Event",
+                      "Hemorrhagic stroke",
+                      "GI bleeding",
+                      
+                      "Any death",
+                      "Dyspnea")
 
-outcomeOrder <- c("Net Adverse Clinical Event", 
-                  
-                  "Ischemic event",
-                  "ischemic stroke",
-                  "Revascularization",
-                  "Acute MI",
-                  
-                  "Hemorrhagic Event",
-                  "hemorrhagic stroke",
-                  "GI bleeding",
-                  
-                  "dyspnea",
-                  "any death")
-
+analysisIds <- analysis$analysisId
+analysisIdsOfInterest <- c(1,2,3,4,7,8,9,10,11,12,13,1142)
+analysisIdOrder <- c(1,2,4,
+                     7,8,13,
+                     10,11,1142,
+                     3,9,12)
 # ##Trim strings for outcome Name 
 # analysisSummary$outcomeName <- gsub("SCYou:","",analysisSummary$outcomeName)
 # analysisSummary$outcomeName <- gsub("outcome","",analysisSummary$outcomeName)
@@ -53,23 +63,27 @@ outcomeOrder <- c("Net Adverse Clinical Event",
 for(analysisIdOfInterest in analysisIds){
     #analysisIdOfInterest <- analysisIds[1]
     #analysisIdOfInterest=6
+    
     data <- analysisSummary %>% 
         filter (analysisId == analysisIdOfInterest) %>%
-        filter (outcomeName %in% outcomeNameOfInterest) %>%
+        filter (outcomeId %in% outcomeIdOfInterest) %>%
         filter (targetId == 874 & comparatorId == 929 )
     
     #reorder the data according to the outcome order
-    data <- data[ match(outcomeNameOfInterest,data$outcomeName),]
+    data <- data[ match(outcomeIdOrder,data$outcomeId),]
+    data$outcomeName <- outcomeNameOrder
     
     tabletext <- cbind(
         c("Outcome","\n",as.character(data$outcomeName)), 
         
-        c(sprintf("Target (n= %d)", data$target[1]),"Event Rate, %/yr",sprintf("%.2f",data$eventsTarget/data$targetDays*100*365) ),
+        c(sprintf("Target (n= %d)", data$targetSubjects[1]),"Event Rate, %/yr",
+          sprintf("%.2f",data$targetOutcomes/data$targetDays*100*365) ),
         #c("\n","Mean followup days", round(data$targetDays/data$target,0)),
         
-        c(sprintf("Comparator (n= %d)", data$comparator[1]),"Event Rate, %/yr",sprintf("%.2f",data$eventsComparator/data$comparatorDays*100*365)),
+        c(sprintf("Comparator (n= %d)", data$comparatorSubjects[1]),"Event Rate, %/yr",
+          sprintf("%.2f",data$comparatorOutcomes/data$comparatorDays*100*365)),
         #c("\n","Mean followup days", round(data$comparatorDays/data$comparator,0)), 
-        c( "HR","(95% CI)", sprintf("%.2f \n (%.2f-%.2f)",data$rr, data$ci95lb, data$ci95ub) ),
+        c( "HR","(95% CI)", sprintf("%.2f \n (%.2f-%.2f)",data$rr, data$ci95Lb, data$ci95Ub) ),
         
         c(paste0("P"," value"),"\n", sprintf("%.3f",data$p) )
     )
@@ -80,8 +94,8 @@ for(analysisIdOfInterest in analysisIds){
                                graph.pos=5, 
                                graphwidth = unit(60,'mm'),
                                mean=c(NA,NA,data$rr), 
-                               lower=c(NA,NA,data$ci95lb), upper=c(NA,NA,data$ci95ub),
-                               title=data$analysisDescription[1],
+                               lower=c(NA,NA,data$ci95Lb), upper=c(NA,NA,data$ci95Ub),
+                               title=analysis$description[analysis$analysisId==analysisIdOfInterest],
                                xlab="<--Ticagrelor Better--  --Clopidogrel Better--->",
                                xlog = TRUE,
                                clip=c(0.5,2.0), 
@@ -103,7 +117,10 @@ for(analysisIdOfInterest in analysisIds){
         )
         dev.off()
     })
-    
+}
+
+#From 2013 to 2015
+for(analysisIdOfInterest in analysisIds){
     #From 2013 to 2015
     data <- analysisSummary %>% 
         filter (analysisId == analysisIdOfInterest) %>%
@@ -154,6 +171,10 @@ for(analysisIdOfInterest in analysisIds){
         )
         dev.off()
     })
+}
+
+#From 2013 to 2015
+for(analysisIdOfInterest in analysisIds){
     
     ##Narrow definition
     data <- analysisSummary %>% 
@@ -210,33 +231,38 @@ for(analysisIdOfInterest in analysisIds){
     
 }
 
-for(outcomeNameSp in outcomeNameOfInterest){
+for(outcomeIdSp in outcomeOfInterest){
     #outcomeIdSp <- outcomeOfInterest[2]
     data <- analysisSummary %>% 
-        filter (outcomeName == outcomeNameSp) %>%
-        filter (analysisId %in% analysisIds) %>%
-        filter (targetId == 874 & comparatorId == 929 )
+        filter (outcomeId == outcomeIdSp) %>%
+        filter (analysisId %in% analysisIdsOfInterest) %>%
+        filter (targetId == 874 & comparatorId == 929 ) 
+    
+    data <- data[ match(analysisIdOrder,data$analysisId),]
     
     tabletext <- cbind(
-        c("Analysis","\n",as.character(data$analysisDescription)), 
+        c("Analysis","\n",as.character(analysis$description[match(analysisIdOrder,analysis$analysisId)])), 
         
-        c("Cumulative outcome \n /Target population", "(%/year)", sprintf("%d/%d \n %.2f", data$eventsTarget,data$target,data$eventsTarget/data$targetDays*100*365)),
+        c(sprintf("Target (n= %d)", data$targetSubjects[1]),"Event Rate, %/yr",
+          sprintf("%.2f",data$targetOutcomes/data$targetDays*100*365) ),
+        #c("\n","Mean followup days", round(data$targetDays/data$target,0)),
         
-        c("Cumulative outcome \n /Comparator population", "(%/year)", sprintf("%d/%d \n %.2f", data$eventsComparator,data$comparator,data$eventsComparator/data$comparatorDays*100*365)),
-        
+        c(sprintf("Comparator (n= %d)", data$comparatorSubjects[1]),"Event Rate, %/yr",
+          sprintf("%.2f",data$comparatorOutcomes/data$comparatorDays*100*365)),
         #c("\n","Mean followup days", round(data$comparatorDays/data$comparator,0)), 
-        c( "HR","(95% CI)", sprintf("%.2f \n (%.2f-%.2f)",data$rr, data$ci95lb, data$ci95ub) ),
+        c( "HR","(95% CI)", sprintf("%.2f \n (%.2f-%.2f)",data$rr, data$ci95Lb, data$ci95Ub) ),
         
         c(paste0("P"," value"),"\n", sprintf("%.3f",data$p) )
     )
+    
     try({
-        tiff(file.path(outputFolder,paste0("forestplot_Outcome",outcomeNameSp,".tiff")), width = 1300, height =600)
+        tiff(file.path(outputFolder,paste0("forestplot_Outcome",outcomes$outcomeName[outcomes$outcomeId==outcomeIdSp],".tiff")), width = 1300, height =600)
         forestplot::forestplot(labeltext=tabletext, 
                                graph.pos=5,
                                graphwidth = unit(60,'mm'),
                                mean=c(NA,NA,data$rr), 
-                               lower=c(NA,NA,data$ci95lb), upper=c(NA,NA,data$ci95ub),
-                               title=data$outcomeName[1],
+                               lower=c(NA,NA,data$ci95Lb), upper=c(NA,NA,data$ci95Ub),
+                               title=outcomes$outcomeName[outcomes$outcomeId==outcomeIdSp],
                                xlab="<--Ticagrelor Better--  --Clopidogrel Better--->",
                                xlog = TRUE,
                                clip=c(0.5,2.0), 
