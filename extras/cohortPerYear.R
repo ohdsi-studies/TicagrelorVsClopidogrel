cdb26c188d4a9696643bb1ef99224621759e3d5e
chandryou <applegna@gmail.com>
2019-06-27 18:14:45 +0900

    add module to calculate cohort per year

--- extras/cohortPerYear.R
+++ extras/cohortPerYear.R
@@ -7,15 +7,14 @@ library(ggplot2)
 # Year <- as.numeric(rep(c("1950","1960","1970","1980","1990","2000","2010"),each=7))
 # Value <- runif(49, 10, 100)
 # data <- data.frame(Sector,Year,Value)
-# 
+#
 # my_fun=function(vec){ as.numeric(vec[3]) / sum(data$Value[data$Year==vec[2]]) *100 }
 # data$prop=apply(data , 1 , my_fun)
-# 
-# ggplot(data, aes(x=Year, y=prop, fill=Sector)) + 
+#
+# ggplot(data, aes(x=Year, y=prop, fill=Sector)) +
 #     geom_area(alpha=0.6 , size=1, colour="black")
 #############################################
 library(dplyr)
-dataFolder
 
 fileList<-list.files(dataFolder, recursive = TRUE)
 countList<-fileList[grepl("cohort_count_per_year.csv",fileList)]
@@ -28,17 +27,17 @@ for (file in countList){
     clo <- count [count$cohortDefinitionId == 929,]
     colnames(tica)[grepl("personCount",colnames(tica))]<-"ticaPersonCount"
     colnames(tica)[grepl("cohortDateSum",colnames(tica))]<-"ticaDateSum"
-    
+
     colnames(clo)[grepl("personCount",colnames(clo))]<-"cloPersonCount"
     colnames(clo)[grepl("cohortDateSum",colnames(clo))]<-"cloDateSum"
-    
+
     mergedCount<-dplyr::full_join(tica,clo,by = "year") %>% arrange(year)
-    
+
     mergedCount$ticaPersonCount[is.na(mergedCount$ticaPersonCount)]<-0
     mergedCount$cloPersonCount[is.na(mergedCount$cloPersonCount)]<-0
     mergedCount$ticagrelorProp <- mergedCount$ticaPersonCount/(mergedCount$ticaPersonCount+mergedCount$cloPersonCount)
-    
-    ggplot(mergedCount, aes(x=year, y=ticagrelorProp)) + 
+
+    ggplot(mergedCount, aes(x=year, y=ticagrelorProp)) +
         geom_area(alpha=0.6 , size=1, colour="black")+
         coord_cartesian(xlim = c(2010:2017), ylim = c(0:1))
 }
